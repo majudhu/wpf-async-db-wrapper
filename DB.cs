1cf79c6ca3b60a62bec07eabf9d7639c1052082a
Majudhu <majudhu.sup@gmail.com>
2020-11-01 09:07:59 +0500

    first commit

---
+++ DB.cs
@@ -0,0 +1,62 @@
+﻿using MySqlConnector;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace db_controller
+{
+    class DB
+    {
+        private readonly string CON_STR;
+        public DB(string connectionString)
+        {
+            CON_STR = connectionString;
+        }
+
+        public async Task<int> nonQuery(string query, Dictionary<string, object> values)
+        {   // UPDATE AND DELETE queries, returns number of rows affected
+            using (var connection = new MySqlConnection(CON_STR))
+            {
+                await connection.OpenAsync();
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    foreach (var value in values)
+                        command.Parameters.AddWithValue(value.Key, value.Value);
+
+                    return await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
+        public async Task<object> scalar(string query, Dictionary<string, object> values)
+        {   // fetch single row single column
+            using (var connection = new MySqlConnection(CON_STR))
+            {
+                await connection.OpenAsync();
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    foreach (var value in values)
+                        command.Parameters.AddWithValue(value.Key, value.Value);
+
+                    return await command.ExecuteScalarAsync();
+                }
+            }
+        }
+        public async Task<DataTable> query(string query, Dictionary<string, object> values)
+        {   // SELECT queries
+            var dataTable = new DataTable();
+            using (var connection = new MySqlConnection(CON_STR))
+            {
+                await connection.OpenAsync();
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    foreach (var value in values)
+                        command.Parameters.AddWithValue(value.Key, value.Value);
+
+                    dataTable.Load(await command.ExecuteReaderAsync());
+                    return dataTable;
+                }
+            }
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace db_controller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DB db;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            txtQuery.Text = "SELECT * FROM customers WHERE mobile = @mobile";
+            txtName.Text = "@mobile";
+            txtValue.Text = "1234";
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            db = new DB(txtConnectionString.Text);
+            lblStatus.Content = "Connection is OPEN";
+            btnNonQuery.IsEnabled = btnScalar.IsEnabled = btnQuery.IsEnabled = true;
+        }
+
+        private async void btnQuery_Click(object sender, RoutedEventArgs e)
+        {
+            var results = await db.query(txtQuery.Text, new Dictionary<string, object> { { txtName.Text, txtValue.Text } });
+            lblStatus.Content = $"row count: {results.Rows.Count}";
+            dgvData.ItemsSource = results.DefaultView;
+        }
+
+        private async void btnScalar_Click(object sender, RoutedEventArgs e)
+        {
+
+            var result = await db.scalar(txtQuery.Text, new Dictionary<string, object> { { txtName.Text, txtValue.Text } });
+            lblStatus.Content = $"result: {result}";
+        }
+
+        private async void btnNonQuery_Click(object sender, RoutedEventArgs e)
+        {
+
+            var result = await db.nonQuery(txtQuery.Text, new Dictionary<string, object> { { txtName.Text, txtValue.Text } });
+            lblStatus.Content = $"updated rows count: {result}";
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            txtQuery.Text = "INSERT INTO customers (mobile) VALUES (@mobile)";
+            txtName.Text = "@mobile";
+            txtValue.Text = "1234";
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            txtQuery.Text = "UPDATE customers SET points = points + 1 WHERE mobile = @mobile";
+            txtName.Text = "@mobile";
+            txtValue.Text = "1234";
+        }
+    }
+}
